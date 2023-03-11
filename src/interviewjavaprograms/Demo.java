08bf1e9b3f9edc650a35bf24d68cacefcf0cfa31
Harsha <Harsha@DESKTOP-PEQHQT4>
2023-03-11 15:56:26 +0400

    Commit Java Presentation

--- src/interviewjavaprograms/Demo.java
+++ src/interviewjavaprograms/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		String str="Welcome to java";
 		int count=1;
-		for(int i=0;i<str.length()-1;i++)
+		for(int i=0;i<
+				str.length()-1;i++)
 		{
 			if((str.charAt(i)==' ')&&(str.charAt(i+1)!=' '))
 			{
